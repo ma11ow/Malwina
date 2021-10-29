0fd47efbdbf7bd2b42aaa1a64c5625c3bbc744d0
ma11ow <s25943@pjwstk.edu.pl>
2021-10-29 13:26:39 +0200

    zad 2.2

---
+++ zad2.2.cpp
@@ -0,0 +1,22 @@
+
+#include <iostream>
+#include<string>
+
+auto main() -> int 
+{
+std::cout  << "Podaj dwie liczby\n";
+
+int a;
+int b;
+int c;
+	std::cin >>a;
+	std::cin >>b;
+	std::cin >>c;
+if (b>a)
+
+ for (int i = a; i<b; ++i)
+if ( i % c == 0 )
+	std::cout<<i<<'\n';
+
+	
+}
