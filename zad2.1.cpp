cc9e6c1449ca2d8f5508332d548fe2ade6cc154d
ma11ow <s25943@pjwstk.edu.pl>
2021-10-29 13:25:31 +0200

    dodany plik 2.1

---
+++ zad2.1.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+#include<string>
+
+auto main() -> int 
+{
+std::cout  << "Podaj dwie liczby\n";
+
+int a;
+int b;
+	std::cin >>a;
+	std::cin >>b;
+if (b>a)
+
+ for (int i = a; i<b; i++)
+
+	std::cout<<i<<'\n';
+
+	
+}
