f6410b9987c5866ca305fe55fca2030ceef5f61f
ma11ow <s25943@pjwstk.edu.pl>
2021-11-05 12:09:12 +0100

    xx

---
+++ 3.11.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include<string>
+
+auto main() -> int 
+{
+std::cout  << "Podaj liczbe mniejsza od 100\n";
+int a;
+std::cin >>a;
+    int tab[a];	
+	for (int c=0; c<a; c++)
+	{ 
+	tab[a] = {0};	
+	std::cout<<tab[a];
+    }
+
+return 0;
+}
+		
