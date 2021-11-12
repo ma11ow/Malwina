69912999a8daaf3a8039ff8e336bf98095e3e5f1
ma11ow <s25943@pjwstk.edu.pl>
2021-11-12 13:12:06 +0100

    bb

---
+++ 1.2.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+#include <string>
+
+auto  main() -> int
+{
+std::cout  << "podaj dwie liczby\n";
+
+	
+	auto  value = std:: string {};
+	auto value2= std:: string {};
+	std:: getline(std::cin , value);
+	std:: getline(std::cin , value2);
+int wynik=std::stoi(value)+std::stoi(value2);
+
+	std::cout << wynik;
+
+}
